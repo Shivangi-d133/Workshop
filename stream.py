6a169cc14df4c6f460ba965cf0808d6a50777de0
Shivangi-d133 <shivangi.desai@ljinstitutes.edu.in>
2025-03-13 13:16:31 +0530

    Add files via upload

---
+++ stream.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("My First Cloud App")
+
+st.write("A simple DataFrame")
+
+df=pd.DataFrame(np.random.randn(10,2),columns=['col1','col2'])
+st.dataframe(df)
+
